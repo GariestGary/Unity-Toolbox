372b3482ff40751023ed6d93723216ce050a0749
GariestGary <rtutede@gmail.com>
2023-06-01 16:13:59 +0300

    save editor base

--- Editor/SaverEditor.cs
+++ Editor/SaverEditor.cs
@@ -14,13 +14,15 @@ namespace VolumeBox.Toolbox
         private SerializedProperty m_slotsCount;
         private SerializedProperty m_slots;
 
+        private Vector2 currentScrollPosition;
+        private const float LABEL_SIZE = 125;
+
         private void OnEnable()
         {
             m_useSaves = serializedObject.FindProperty("useSaves");
             m_stateProvider = serializedObject.FindProperty("stateProvider");
             m_fileHandler = serializedObject.FindProperty("fileHandler");
             m_slotsCount = serializedObject.FindProperty("saveSlotsCount");
-            m_slots = serializedObject.FindProperty("saveSlots");
         }
 
         public override void OnInspectorGUI()
@@ -28,16 +30,30 @@ namespace VolumeBox.Toolbox
             EditorGUILayout.BeginVertical();
 
             EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField("Use Saves");
-            GUILayout.FlexibleSpace();
+            EditorGUILayout.LabelField("Use Saves", GUILayout.Width(LABEL_SIZE));
             m_useSaves.boolValue = EditorGUILayout.Toggle(m_useSaves.boolValue, GUILayout.Width(25));
-
+            GUILayout.FlexibleSpace();
             EditorGUILayout.EndHorizontal();
 
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("State Provider", GUILayout.Width(LABEL_SIZE));
+            EditorGUILayout.PropertyField(m_stateProvider, GUIContent.none);
+            EditorGUILayout.EndHorizontal();
 
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("File Handler", GUILayout.Width(LABEL_SIZE));
+            EditorGUILayout.PropertyField(m_fileHandler, GUIContent.none);
+            EditorGUILayout.EndHorizontal();
 
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Save Slots Count", GUILayout.Width(LABEL_SIZE));
+            m_slotsCount.intValue = EditorGUILayout.IntField(m_slotsCount.intValue, GUILayout.Width(25));
+            GUILayout.FlexibleSpace();
+            EditorGUILayout.EndHorizontal();
 
             EditorGUILayout.EndVertical();
         }
+
+
     }
 }
