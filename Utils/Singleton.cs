9f85742a94ce7aecb4be34bd328212b4def860de
GariestGary <rtutede@gmail.com>
2023-10-11 14:56:37 +0300

    info & singleton fix
    info's message box and hint box references in main scene fixed
    singgleton destroyed flag removed

--- Utils/Singleton.cs
+++ Utils/Singleton.cs
@@ -8,7 +8,6 @@ namespace VolumeBox.Toolbox
     {
         private static T instance;
         private static object lockObject = new object();
-        private static bool destroyed = false;
 
         public static bool HasInstance => instance != null;
 
@@ -16,8 +15,6 @@ namespace VolumeBox.Toolbox
         { 
             get
             {
-                if (destroyed) return null;
-                
                 lock (lockObject)
                 {
                     if (instance == null)
@@ -38,11 +35,6 @@ namespace VolumeBox.Toolbox
         public static void DontDestroy()
         {
             DontDestroyOnLoad(Instance.gameObject);
-        }
-
-        private void OnDestroy()
-        {
-            destroyed = true;
         }
     }
 
